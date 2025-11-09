8c9aa2b8c063651f056c36ce76348c88ec972768
giabinh337 <giabinh337@gmail.com>
2025-11-09 21:53:14 +0700

    bai 15

---
+++ fpt 1/index.js
@@ -0,0 +1,62 @@
+
+let listFilm = [
+  {
+    id: 1,
+    name: "Mưa đỏ",
+    banner: "640x396-muado.jpg",  
+    trailer: "https://www.youtube.com/embed/AJ7JJezZWVE",
+    nation: "Việt Nam"
+  },
+  {
+    id: 2,
+    name: "Thiên địa kiếm tâm",
+    banner: "banner tdkt.jfif",
+    trailer: "https://www.youtube.com/embed/AJ7JJezZWVE",
+    nation: "Trung Quốc"
+  },
+  {
+    id: 3,
+    name: "Thiên địa kiếm tâm",
+    banner: "banner tdkt.jfif",
+    trailer: "https://www.youtube.com/embed/AJ7JJezZWVE",
+    nation: "Trung Quốc"
+  },
+  {
+    id: 4,
+    name: "Thiên địa kiếm tâm",
+    banner: "banner tdkt.jfif",
+    trailer: "https://www.youtube.com/embed/AJ7JJezZWVE",
+    nation: "Trung Quốc"
+  },{
+    id: 5,
+    name: "Thiên địa kiếm tâm",
+    banner: "banner tdkt.jfif",
+    trailer: "https://www.youtube.com/embed/AJ7JJezZWVE",
+    nation: "Trung Quốc"
+  },
+];
+
+let bannerElement = document.getElementById('banner');
+let bannerImg = document.getElementById('bannerImg');
+let titleElement = document.getElementsByClassName('title')[0];
+let nationElement = document.getElementsByClassName('nation')[0];
+let trailerElement = document.getElementById('trailer');
+let currentFilm = null;
+function chooseFilm(filmId) {
+  let selectedFilm = listFilm.find(film => film.id === filmId);
+  if (!selectedFilm) return;
+
+  currentFilm = selectedFilm;
+
+  trailerElement.style.display = "none";
+  bannerElement.style.display = "block";
+
+  bannerImg.src = selectedFilm.banner;
+
+  titleElement.textContent = selectedFilm.name;
+  nationElement.textContent = " -"+ selectedFilm.nation;
+}
+function viewTrailer() {
+  bannerElement.style.display = "none";
+  trailerElement.style.display = "block";
+}
